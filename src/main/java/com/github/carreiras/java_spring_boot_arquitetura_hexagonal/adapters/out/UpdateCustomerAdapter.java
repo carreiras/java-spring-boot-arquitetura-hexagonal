7e8a00dd3276523fd896a5a082276d81d785175c
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-08-12 10:43:24 -0300

    CRUD UPDATE - Criação do adapter

---
+++ src/main/java/com/github/carreiras/java_spring_boot_arquitetura_hexagonal/adapters/out/UpdateCustomerAdapter.java
@@ -0,0 +1,24 @@
+package com.github.carreiras.java_spring_boot_arquitetura_hexagonal.adapters.out;
+
+import com.github.carreiras.java_spring_boot_arquitetura_hexagonal.adapters.out.repository.CustomerRepository;
+import com.github.carreiras.java_spring_boot_arquitetura_hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.github.carreiras.java_spring_boot_arquitetura_hexagonal.application.core.domain.Customer;
+import com.github.carreiras.java_spring_boot_arquitetura_hexagonal.application.ports.out.UpdateCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCustomerAdapter implements UpdateCustomerOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
